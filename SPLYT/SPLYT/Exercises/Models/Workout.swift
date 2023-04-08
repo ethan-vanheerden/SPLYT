6eb305b14faad51dc757066c24380689c22313fd
evh4 <ethan@vpoint.biz>
2023-04-07 20:27:51 -0400

    fixes

--- SPLYT/SPLYT/Exercises/Models/Workout.swift
+++ SPLYT/SPLYT/Exercises/Models/Workout.swift
@@ -8,26 +8,9 @@
 import Foundation
 
 /// A created workout. This same struct is used for workouts that are planned, in-progress, or completed.
-final class Workout: Codable, Equatable {
+struct Workout: Codable, Equatable {
     let id: String
-    var name: String
-    var exerciseGroups: [ExerciseGroup]
+    let name: String
+    let exerciseGroups: [ExerciseGroup]
     var lastCompleted: Date?
-    
-    init(id: String,
-         name: String,
-         exerciseGroups: [ExerciseGroup],
-         lastCompleted: Date? = nil) {
-        self.id = id
-        self.name = name
-        self.exerciseGroups = exerciseGroups
-        self.lastCompleted = lastCompleted
-    }
-    
-    static func == (lhs: Workout, rhs: Workout) -> Bool {
-        return lhs.id == rhs.id &&
-        lhs.name == rhs.name &&
-        lhs.exerciseGroups == rhs.exerciseGroups &&
-        lhs.lastCompleted == rhs.lastCompleted
-    }
 }
--- SPLYT/SPLYT/Swift Packages/DesignSystem/Tests/DesignSystemTests/Atomic/Molecules/DialogTests.swift
+++ SPLYT/SPLYT/Swift Packages/DesignSystem/Tests/DesignSystemTests/Atomic/Molecules/DialogTests.swift
@@ -4,19 +4,6 @@ import DesignSystem
 import SwiftUI
 
 final class DialogTests: XCTestCase {
-    func testMenuItem() {
-        let viewState = MenuItemViewState(title: "TITLE")
-        let subtitleViewState = MenuItemViewState(title: "TITLE",
-                                                  subtitle: "THIS IS A SUBTITLE")
-        let view = VStack {
-            MenuItem(viewState: viewState) { _ in }
-            MenuItem(viewState: subtitleViewState) { _ in }
-            Spacer()
-        }
-        let vc = UIHostingController(rootView: view)
-        assertSnapshot(matching: vc, as: .image(on: .mediumImage()))
-    }
-    
     func testDialog_JustTitle() {
         let viewState = DialogViewState(title: "Dialog title",
                                         primaryButtonTitle: "Ok")
@@ -25,7 +12,7 @@ final class DialogTests: XCTestCase {
             Text("Hello, world!")
         }.dialog(isOpen: true, viewState: viewState, primaryAction: { })
         let vc = UIHostingController(rootView: view)
-        assertSnapshot(matching: vc, as: .image(on: .smallImage()))
+        assertSnapshot(matching: vc, as: .image(on: .mediumImage()))
     }
     
     func testDialog_Complex() {
