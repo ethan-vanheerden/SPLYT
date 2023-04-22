30c3d59ffd015b236c6e943bf9fa768f6b6b463c
evh4 <ethan@vpoint.biz>
2023-04-22 17:05:51 -0400

    missing files

--- SPLYT/SPLYT/Swift Packages/DesignShowcase/Sources/DesignShowcase/Gallery/Views/Atoms/TagGallery.swift
+++ SPLYT/SPLYT/Swift Packages/DesignShowcase/Sources/DesignShowcase/Gallery/Views/Atoms/TagGallery.swift
@@ -5,13 +5,9 @@ struct TagGallery: View {
     var body: some View {
         VStack(spacing: Layout.size(1)) {
             Spacer()
-//            Tag(text: "DROP SET", fill: Color(splytColor: .red))
-//            Tag(text: "REST PAUSE", fill: Color(splytColor: .green))
-//            Tag(text: "ECCENTRIC", fill: Color(splytColor: .yellow))
-//            Tag(text: "UNICORN VOMIT", fill: AngularGradient(colors: [.red, .green, .blue, .purple, .pink],
-//                                                             center: .center,
-//                                                             startAngle: .degrees(90),
-//                                                             endAngle: .degrees(360)))
+            Tag(viewState: TagViewState(title: "Drop Set", color: .green))
+            Tag(viewState: TagViewState(title: "Rest/Pause", color: .gray))
+            Tag(viewState: TagViewState(title: "Eccentric", color: .red))
             Spacer()
         }
         .padding(.horizontal)
